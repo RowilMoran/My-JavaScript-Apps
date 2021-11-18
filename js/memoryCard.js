97454dc19d09d479f3cca29a2d2a10af336ac8da
RowilMoran <rowilmoran@gmail.com>
2021-11-18 15:49:44 -0300

    TeenTitansMemoryCArd

---
+++ js/memoryCard.js
@@ -0,0 +1,241 @@
+//lets Grap html tags and classes
+const cardsContainer = document.querySelector(".cards-container");
+const playerLivesCount = document.querySelector(".lives-value");
+const gameContainer = document.querySelector(".game-container")
+
+let lives = 17;
+
+//link lives to html
+playerLivesCount.innerHTML = lives;
+
+//generate the data(cards imgs)
+
+const getImgs = () => 
+[
+    {imgSrc: "./Resources/memoryCard/bee.png", name: "bee"},
+    {imgSrc: "./Resources/memoryCard/blackfire.png", name: "blackfire"},
+    {imgSrc: "./Resources/memoryCard/cybor.png",  name: "cyborg"},
+    {imgSrc: "./Resources/memoryCard/beast.png",name: "beast"},
+    {imgSrc: "./Resources/memoryCard/raven.png", name: "raven"},
+    {imgSrc: "./Resources/memoryCard/robin.png", name: "robin"},
+    {imgSrc: "./Resources/memoryCard/starfire.png", name: "starfire"},
+    {imgSrc: "./Resources/memoryCard/rose.png", name: "rose"},
+    {imgSrc: "./Resources/memoryCard/bee.png", name: "bee"},
+    {imgSrc: "./Resources/memoryCard/blackfire.png", name: "blackfire"},
+    {imgSrc: "./Resources/memoryCard/cybor.png",  name: "cyborg"},
+    {imgSrc: "./Resources/memoryCard/beast.png",name: "beast"},
+    {imgSrc: "./Resources/memoryCard/raven.png", name: "raven"},
+    {imgSrc: "./Resources/memoryCard/robin.png", name: "robin"},
+    {imgSrc: "./Resources/memoryCard/starfire.png", name: "starfire"},
+    {imgSrc: "./Resources/memoryCard/rose.png", name: "rose"}
+];
+
+// pro images 
+
+const getProImgs = () => 
+[
+    {imgSrc: "./Resources/memoryCard/bee.png", name: "bee"},
+    {imgSrc: "./Resources/memoryCard/blackfire.png", name: "blackfire"},
+    {imgSrc: "./Resources/memoryCard/cybor.png",  name: "cyborg"},
+    {imgSrc: "./Resources/memoryCard/beast.png",name: "beast"},
+    {imgSrc: "./Resources/memoryCard/raven.png", name: "raven"},
+    {imgSrc: "./Resources/memoryCard/robin.png", name: "robin"},
+    {imgSrc: "./Resources/memoryCard/starfire.png", name: "starfire"},
+    {imgSrc: "./Resources/memoryCard/rose.png", name: "rose"},
+    {imgSrc: "./Resources/memoryCard/bee.png", name: "bee"},
+    {imgSrc: "./Resources/memoryCard/blackfire.png", name: "blackfire"},
+    {imgSrc: "./Resources/memoryCard/cybor.png",  name: "cyborg"},
+    {imgSrc: "./Resources/memoryCard/beast.png",name: "beast"},
+    {imgSrc: "./Resources/memoryCard/raven.png", name: "raven"},
+    {imgSrc: "./Resources/memoryCard/robin.png", name: "robin"},
+    {imgSrc: "./Resources/memoryCard/starfire.png", name: "starfire"},
+    {imgSrc: "./Resources/memoryCard/rose.png", name: "rose"},
+    //doble the images
+    {imgSrc: "./Resources/memoryCard/bad-boy-1.png", name: "bad-boy-1"},
+    {imgSrc: "./Resources/memoryCard/bad-boy-2.png", name: "bad-boy-2"},
+    {imgSrc: "./Resources/memoryCard/ciclo.png",  name: "ciclo"},
+    {imgSrc: "./Resources/memoryCard/flash-boy.png",name: "flash-boy"},
+    {imgSrc: "./Resources/memoryCard/gizmo.png", name: "gizmo"},
+    {imgSrc: "./Resources/memoryCard/hive.png", name: "hive"},
+    {imgSrc: "./Resources/memoryCard/pet.png", name: "pet"},
+    {imgSrc: "./Resources/memoryCard/robin-2.png", name: "robin-2"},
+    {imgSrc: "./Resources/memoryCard/bad-boy-1.png", name: "bad-boy-1"},
+    {imgSrc: "./Resources/memoryCard/bad-boy-2.png", name: "bad-boy-2"},
+    {imgSrc: "./Resources/memoryCard/ciclo.png",  name: "ciclo"},
+    {imgSrc: "./Resources/memoryCard/flash-boy.png",name: "flash-boy"},
+    {imgSrc: "./Resources/memoryCard/gizmo.png", name: "gizmo"},
+    {imgSrc: "./Resources/memoryCard/hive.png", name: "hive"},
+    {imgSrc: "./Resources/memoryCard/pet.png", name: "pet"},
+    {imgSrc: "./Resources/memoryCard/robin-2.png", name: "robin-2"}
+]
+
+let easyImgs = getImgs();
+let proImgs = getProImgs();
+let arraySelected = [];
+
+
+//randomize the images 
+const random = (array) => {
+    array.sort(() => Math.random()- 0.5);
+    return  array;
+}
+
+
+
+//choose difficulty 
+document.addEventListener("click", e => {
+    let difficulty = document.querySelector('input[name="difficulty"]:checked').value
+    if (e.target.matches("#btn")) {
+        if (difficulty === "easy"){
+            arraySelected = easyImgs;
+            random(arraySelected);
+            gameContainer.classList.add("show");
+            cardGenerator();
+        } else if (difficulty === "pro") {
+            arraySelected = proImgs;
+            random(arraySelected);
+            gameContainer.classList.add("show");
+            cardGenerator();
+        }
+    }
+})
+
+
+
+
+//generate cards in html
+const cardGenerator = () => {
+    //start the iteration to create elements of each image.
+    arraySelected.forEach((item) => {
+        let card = document.createElement("div");
+        let front = document.createElement("img");
+        let back = document.createElement("div");
+        
+        //add classes and src to elements
+        card.classList.add("card");
+        front.src = item.imgSrc;
+        front.classList.add("front")
+        back.classList.add("back");
+        //set the name of the image to a "name" atribute in card div
+        card.setAttribute("name", item.name)
+        //insert new elements in the DOM
+        cardsContainer.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
+        
+        //clock events when the card is clicked
+        card.addEventListener("click", (e) => {
+            card.classList.add("rotate");
+            //this class is generated to compare the two clicked cards 
+            card.classList.add("flipped");
+            //invoque a check card function
+            checkCards(e)
+        });  
+    });
+    //insert at dom depend of the difficulty selected
+    if (arraySelected.length === 16) {
+        console.log("im on easy")
+        cardsContainer.classList.add("easy");
+    } else {
+        cardsContainer.classList.add("pro");
+        console.log("im onpro")
+    }
+}
+
+
+
+//this function compare the two clicked cards.
+const checkCards = e => {
+    
+    //when a card is clicked, flipped class is added. we are grabbing them.
+    const flippedCards = document.querySelectorAll(".flipped");
+    //
+    const rotate = document.querySelectorAll(".rotate");
+    
+    //compare the flipped cards
+    if (flippedCards.length === 2) {
+        //check match
+        if(flippedCards[0].getAttribute("name") === flippedCards[1].getAttribute("name")) {
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents = "none";
+            })
+        } else { 
+            //if they dont match
+            flippedCards.forEach((card) => {
+               card.classList.remove("flipped");
+               setTimeout(() => {
+                card.classList.remove("rotate");
+               },900)
+           });
+
+           //update lives
+           lives --;
+            playerLivesCount.innerHTML = lives;
+            //reset the game after loosing.
+            if (lives === 0) {
+                let lostPopUp = document.querySelector(".lost");
+                lostPopUp.classList.add("show");
+                setTimeout(() => {
+                    lostPopUp.classList.remove("show");
+                    gameContainer.classList.remove("show");
+                    reset();
+                }, 3000);
+            }
+        }
+    } 
+
+    if(arraySelected.length === 16) {
+        if (rotate.length === 16) {
+            let winGame = document.querySelector(".win");
+            winGame.classList.add("show");
+            setTimeout(() => {
+                reset();
+                winGame.classList.remove("show");
+                gameContainer.classList.remove("show");
+            }, 4000);
+        }
+    } else if (arraySelected.length === 32){
+        if (rotate.length === 32) {
+            let winGame = document.querySelector(".win");
+            winGame.classList.add("show");
+            setTimeout(() => {
+                reset();
+                gameContainer.classList.remove("show");
+                winGame.classList.remove("show");
+            }, 4000);
+        }
+    }
+
+   
+}
+
+
+//reset function when lost and won the game
+const reset = () => {
+    
+    
+    let card= document.querySelectorAll(".card");
+    let faces = document.querySelectorAll(".front");
+    cardsContainer.classList.remove("easy");
+    cardsContainer.classList.remove("pro");
+    cardsContainer.textContent = "";
+    
+    
+    //place the images randomly in the DOM with the respective name.
+    arraySelected.forEach((item, index) => {
+
+        card[index].classList.remove("rotate");
+        card[index].style.pointerEvents ="all";
+        card[index].setAttribute("name", item.name);
+        faces[index].src = item.imgSrc;
+       
+    });
+
+    lives = 17;
+
+    playerLivesCount.textContent = lives;
+   
+}
+
+
---
+++ js/snake.js
@@ -0,0 +1,174 @@
+//we start by getting the canvas from the html
+
+const canvas = document.getElementById("canvas");
+
+//then we create a drawing object for the canva
+
+const ctx = canvas.getContext("2d");
+
+//getting the popup we are using when loosing the game
+const popup = document.getElementById("pop-up");
+
+//we load our img and sound
+let imageName = new Image();
+imageName.src = "Resources/snake/mango.png"
+let audioEat = new Audio();
+audioEat.src = "Resources/snake/sound.mp3";
+let groundImg = new Image();
+groundImg.src = "Resources/snake/ground.png";
+let crashSound = new Audio();
+crashSound.src = "Resources/snake/crash.mp3"
+
+//save the score in a variable
+
+let score = 0;
+
+//we make a unit named "box" that represent 32px
+
+let box = 32;
+
+//we save the snake coords in arrays to access later in draw function
+
+let snake = [];
+snake[0] = {x: 9*box, y: 10*box};
+
+//we randomly create the value of the food coordnates
+
+let food = {
+    x:Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box
+}
+
+//control the snake
+
+let d;
+
+function direction(event) {
+    if (event.keyCode === 37 && d != "RIGHT") {
+        d = "LEFT";
+    } else if (event.keyCode === 38 && d !== "DOWN") {
+        d = "UP";
+    } else if (event.keyCode === 39 && d !== "LEFT") {
+        d = "RIGHT";
+    } else if (event.keyCode === 40 && d !== "UP"){
+        d = "DOWN";
+    }
+} 
+
+
+//check colission 
+
+function collision(head,array){
+    for(let i = 0; i < array.length; i++){
+        if(head.x == array[i].x && head.y == array[i].y){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+//we create a draw function
+
+function draw() {
+ 
+    // place the background of the canva
+    ctx.drawImage(groundImg, 0, 0);
+
+    // loop through the array with conditionals to identify the head and the body of the snake
+    for(let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (i===0) ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box)
+        ctx.strokeStyle =  "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    //Place food in canva
+    ctx.drawImage(imageName, food.x, food.y);
+
+    //old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //then we create an eventListener for keyDown
+
+    document.addEventListener("keydown", direction);
+
+    //wich direction
+
+    if (d === "LEFT") snakeX -= box;
+    if (d === "RIGHT") snakeX += box;
+    if (d === "UP") snakeY -= box;
+    if (d === "DOWN") snakeY += box;
+
+   
+    // when the snake eat 
+
+    if (snakeX === food.x && snakeY === food.y)  {
+        score++;
+        audioEat.play()
+        food = {
+            x:Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 15 + 3) * box
+        }
+    } else {
+        //remove the tail
+        snake.pop();
+    }
+
+    //new head
+    
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    
+    
+    //game over
+    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
+        clearInterval(snakeGame);
+        crashSound.play();
+        popup.classList.toggle("show");
+        //reload the page after 2.5seg
+        setTimeout(() => {
+            location.reload();
+        }, 2500);
+    }
+    
+    //add new head
+    snake.unshift(newHead);
+
+
+    //create the score in top of canva
+    ctx.fillStyle = "white";
+    ctx.font =  "45px Changa One";
+    ctx.fillText(score, 2.4*box, 1.6 * box);
+
+}
+
+
+//select the speed of the snake
+const btn = document.getElementById("btn")
+let level = document.getElementsByName('level');
+let interval;
+let snakeGame;
+
+btn.addEventListener("click", e => {
+    e.preventDefault()
+    for (let i = 0; i < level.length; i++) {
+        if (level[i].checked) {
+            interval = (level[i].value);
+            snakeGame = setInterval(draw,interval)
+        }
+    } 
+    //Show the canva on screen
+    document.querySelector(".canvas").classList.toggle("show")
+})
+
+
+
+
+
+
+
+
