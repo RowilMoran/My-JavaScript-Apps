2ad61bdbc7c8e408922654c9acef6366b3a64b10
RowilMoran <rowilmoran@gmail.com>
2021-12-17 21:10:51 -0300

    begin of pagination

--- js/movieSearch.js
+++ js/movieSearch.js
@@ -81,7 +81,7 @@ async function gliderData() {
                     (movie.poster_path === null) ? posterImg.setAttribute("src", "Resources/movieSearch/not-abilable.jpg") : posterImg.setAttribute("src", posterUrl);
                     discoverSection.appendChild(posterImg);
                     discoverMore();
-                })
+                });
             }
         } catch (error){
             let message = error.statusText || "Not found";
@@ -110,6 +110,7 @@ async function gliderData() {
     document.addEventListener("DOMContentLoaded", e => {
         gliderData();
         showContent();
+        searchMovie();
     });
 
 
@@ -181,37 +182,3 @@ function discoverMore() {
 
 
 
-searchMovie()
-
-/* 
-var loadMore = document.getElementById("loadMore");
-maxItems = 4;
-loadItems = 4;
-hiddenClass = "hiddenStyle";
-hiddenItems = Array.from(document.querySelectorAll(".hiddenStyle"));
-
-items.forEach(function (item, index) {
-  console.log(item.innerText, index);
-  if (index > maxItems - 1) {
-    item.classList.add(hiddenClass);
-  }
-});
-
-loadMore.addEventListener("click", function () {
-  [].forEach.call(document.querySelectorAll("." + hiddenClass), function (
-    item,
-    index
-  ) {
-    if (index < loadItems) {
-      item.classList.remove(hiddenClass);
-    }
-
-    if (document.querySelectorAll("." + hiddenClass).length === 0) {
-      loadMore.style.display = "none";
-    }
-  });
-});
-
-
-
-*/
